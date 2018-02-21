0d5258f4f0ad95f0c6fa4c0b7b51e695f8777ed3
Jovanny Martis <jmartis@users.noreply.github.com>
2018-02-21 12:20:30 +0100

    Update globalCustomization.js

--- global/globalCustomization.js
+++ global/globalCustomization.js
@@ -1,12 +1,15 @@
 if(typeof(dojo) != "undefined") {
-    require(["dojo/domReady!"], function() {
-        dojo.place(
+    
+    dojo.addOnLoad(function(){
+       dojo.place(
         "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/global/globalCustomization.css?repoName=ibmcndevsq\"></link>",
         dojo.doc.getElementsByTagName("head")[0].lastChild,
         "after"
     );
         
-        console.log("finished loading globalCustomization.css");
+     console.log("finished loading globalCustomization.css--");
+  
 });
+  
    
 }
